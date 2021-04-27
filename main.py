47b27d457fe381b48ffa5826b6d969f3d464b130
Denys Chernyshov <den.blackshov@gmail.com>
2021-04-27 21:38:49 +0200

    It works!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.action.OpenAction import OpenAction
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
@@ -26,23 +27,28 @@ class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
         items = []
         query = event.get_argument()
-
+        logging.debug(query)
         keyword = event.get_keyword()
+        logging.debug(keyword)
         # Find the keyword id using the keyword (since the keyword can be changed by users)
         keyword_id = None
         for kw_id, kw in list(extension.preferences.items()):
             if kw == keyword:
                 keyword_id = kw_id
-
+        logging.debug(keyword_id)
+        if query is None or len(query) < 3:
+            return
         if keyword_id == "multitran_de_ru":
+            logging.debug(f"LOOKING UP {query}")
             response = mt.get_translations("DE", "RU", query.strip())
+            logging.debug(f"RESPONSE:{response}")
             for t in response['translations']:
                 translation = t['translation']
-            items.append(
-                ExtensionResultItem(
-                    icon='images/icon.png',
-                    name=translation,
-                    on_enter=response['url']))
+                items.append(
+                    ExtensionResultItem(
+                        icon='images/icon.png',
+                        name=translation,
+                        on_enter=OpenAction(response['url'])))
         return RenderResultListAction(items)
 
 if __name__ == '__main__':
